993e2fb36c25b04d11e3776ff18301b5f7e206bd
Cyril Goldenschue <cyril.goldenschue@gmail.com>
2025-01-07 11:49:12 +0100

    add close client after receive success message send

--- src_bonus/client_bonus.c
+++ src_bonus/client_bonus.c
@@ -6,7 +6,7 @@
 /*   By: cgoldens <cgoldens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 13:59:13 by cgoldens          #+#    #+#             */
-/*   Updated: 2024/11/25 14:22:20 by cgoldens         ###   ########.fr       */
+/*   Updated: 2025/01/07 11:48:56 by cgoldens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,10 @@ void	send_msg(pid_t sv_pid, char *msg)
 void	sig_handler(int signum)
 {
 	if (signum == SIGUSR2)
+	{
 		write(1, "Message has been sucessfully receieved!\n", 41);
+		exit (0);
+	}
 }
 
 void	config_signals(void)
